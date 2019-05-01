df882ec4f65d62c1043b0f3dbd1db52657224986
bolein <bolein95@gmail.com>
2019-04-30 21:14:17 -0500

    t-sne update

--- app.R
+++ app.R
@@ -8,27 +8,9 @@ library(cluster)
 library(ggplot2)
 library(plotly)
 
-set.seed(1)
-dfs <- read.csv('TransfromedData_1_sample.csv', header = T)
 
-# calculate distance for variables of all types
-gower_dist <- dfs %>% select(-c(loan_status, addr_state)) %>% daisy(
-  metric = "gower")
-summary(gower_dist)
-
-tsne_obj <- Rtsne(gower_dist, is_distance = TRUE, perplexity=50)
-
-tsne_data <- tsne_obj$Y %>%
-  data.frame() %>%
-  setNames(c("x_coord", "y_coord")) %>%
-  cbind(dfs)
-
-tsne_obj_3d <- Rtsne(gower_dist, is_distance = TRUE, perplexity=50, dims=3)
-
-tsne_data_3d <- tsne_obj_3d$Y %>%
-  data.frame() %>%
-  setNames(c("x_coord", "y_coord", "z_coord")) %>%
-  cbind(dfs)
+tsne_data <- read.csv('space_2d.csv', header = T)
+space <- read.csv('space_3d.csv', header = T)
 
 
 ui <- fluidPage(
@@ -69,13 +51,54 @@ server <- function(input, output) {
   })
   
   output$plot2 <- renderPlotly({
-    plot_ly(tsne_data_3d, x = ~x_coord, y = ~y_coord, z = ~z_coord, 
-            color = ~loan_status, 
-            shape = ~purpose,
-            mode='markers',
-            hoverinfo = 'text',
-            text = ~paste('Purpose', dfs$purpose) # TODO display all relevant data
-    )
+    p <- plot_ly() %>% add_trace(x = space$x_coord, y = space$y_coord, z = space$z_coord, 
+                                 mode='markers',
+                                 hoverinfo = 'text',
+                                 marker=list(
+                                   color = space$cluster_default_rate, 
+                                   colorscale='RdYlBu',
+                                   colorbar=list(title='Default rate')
+                                 ),
+                                 text = ~paste('</br>Cluster', space$cluster, 
+                                               '</br>Cluster default rate:', space$cluster_default_rate, 
+                                               '</br>Purpose', space$purpose), # TODO display all relevant data,
+                                 type = "scatter3d"
+    ) 
+    
+    new_data_point <- space[space$test == T, ][5,]
+    
+    p %>% layout(scene = list(
+      aspectratio = list(
+        x = 1,
+        y = 1,
+        z = 1
+      ),
+      camera = list(
+        center = list(
+          x = 0,
+          y = 0,
+          z = 0
+        ),
+        eye = list(
+          x = 1.96903462608,
+          y = -1.09022831971,
+          z = 0.405345349304
+        ),
+        up = list(
+          x = 0,
+          y = 0,
+          z = 1
+        )
+      ), annotations = list(list(
+        showarrow = T,
+        # datapoint coordinates:
+        x = new_data_point$x_coord,
+        y = new_data_point$y_coord,
+        z = new_data_point$z_coord,
+        text = "New loan application",
+        xshift = 0,
+        opacity = 1
+      ))))
   })
   
   output$brush_info <- renderDataTable({
